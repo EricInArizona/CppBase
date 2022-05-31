a409c005622a9391fbd8741314d288a8d431da0e
Eric Chauvin <echauvin823@gmail.com>
2022-05-31 13:06:48 -0700

    Add files via upload

---
+++ Memory.h
@@ -0,0 +1,37 @@
+// Copyright Eric Chauvin 2022
+
+
+// This is licensed under the GNU General
+// Public License (GPL).  It is the
+// same license that Linux has.
+// https://www.gnu.org/licenses/gpl-3.0.html
+
+// In the cpp file:
+// #include <string.h>
+
+
+// memset
+// strlen() finds the length of a C string.
+
+// memcpy( void* dest, const void * source, howmany )
+
+
+
+// If I need to use Shared Memory Files then
+// this would be just an #include to one for
+// Windows and one for Linux.  And it would
+// be a different class called SharedMem.
+
+
+
+#pragma once
+
+
+#include "BasicTypes.h"
+
+
+class Memory
+  {
+  private:
+
+  };
